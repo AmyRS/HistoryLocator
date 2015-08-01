312ef7b3b4f62fa3a590f7e01add45a407317de6
Amy Sorensen <amyrosesorensen@gmail.com>
2015-08-01 00:22:15 -0500

    Exported CSV Template Fix
    Brought Exported CSV in line with template from HistoryPin

---
+++ newCSV.js
@@ -0,0 +1,22 @@
+/**
+ * @author Amy
+ */
+ 	
+ function runNewCSV(){	
+ 	//Delete local Storage data
+	localStorage.clear();
+		
+	//store template CSV in local
+	var headerData = 'File name*,Photo title (max 50 characters)*,Keywords (max 500 characters),Date (See instructions for format),"\ Interval (Years | 1, 2, 3, 5, 10, 15)\","\ Latitude , Longitude\",Story ,Use Date (TRUE or FALSE),Date Range? (Y or N),Licence (See instructions for options),Copyright holder,Author,Original Link  ,Repository or Archive ,Notes';
+
+	//Puts into local
+ 	alert(headerData);	 			 	    
+ 	//var localNameData = JSON.parse(localStorage.getItem('nameData'));
+  	//var dataToStore = localNameData+", "+XdataToStore+", "+YdataToStore+"\n";				
+  	localStorage.setItem('oneFile',headerData);
+  					
+  	document.getElementById("storeData").disabled = true;
+	
+	//This will make it all work?
+	return true;
+  		};
