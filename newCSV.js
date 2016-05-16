cd015ce789ed91099ed1842ec5147971c527f591
Amy Sorensen <amyrosesorensen@gmail.com>
2016-05-15 20:38:16 -0500

    Fixed a pesky issue of (). Local cache should now behave properly-a

--- newCSV.js
+++ newCSV.js
@@ -7,10 +7,9 @@
 	localStorage.clear();
 		
 	//store template CSV in local
-	var headerData = 'File name*,Photo title (max 50 characters)*,Keywords (max 500 characters),Date (See instructions for format),"\ Interval (Years | 1, 2, 3, 5, 10, 15)\","\ Latitude , Longitude\",Story ,Use Date (TRUE or FALSE),Date Range? (Y or N),Licence (See instructions for options),Copyright holder,Author,Original Link  ,Repository or Archive ,Notes';
+	var headerData = 'File name*,Photo title (max 50 characters)*,Keywords (max 500 characters),Date (See instructions for format),"\ Interval (Years | 1, 2, 3, 5, 10, 15)\","\ Latitude , Longitude\",Story ,Use Date (TRUE or FALSE),Date Range? (Y or N),License (See instructions for options),Copyright holder,Author,Original Link  ,Repository or Archive ,Notes';
 	var headerData = headerData + '\n';
 	
-
 	//Puts into local 			 	    
  	//var localNameData = JSON.parse(localStorage.getItem('nameData'));
   	//var dataToStore = localNameData+", "+XdataToStore+", "+YdataToStore+"\n";				
